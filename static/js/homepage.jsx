0ff757528b48157dcb1d4ca16d9b3d51385d8d3c
Enid-Sky <enid.sky@protonmail.com>
2020-10-23 13:54:11 -0700

    add fragment to component

--- static/js/homepage.jsx
+++ static/js/homepage.jsx
@@ -1,11 +1,31 @@
 "use strict";
 
-function Homepage() {
-  return (
-    <div>
-      This component needs to be implemented.
-    </div>
-  );
+// // component
+// function Homepage() {
+//   return (
+//     <div>
+//       <h2>Name: {props.name}</h2>
+//     </div>
+//   );
+// }
+
+// // component to render
+// ReactDOM.render(<Homepage />, document.querySelector('#app'));
+
+
+
+// component
+class Homepage extends React.Component {
+    render() {
+        return (
+            <div class="welcome">
+              <h2>"My name is Balloonicorn, let's play!"</h2>
+              <a href="/cards"> Cards page </a>
+              <img src="/static/img/balloonicorn.jpg"></img>
+            </div>
+        );
+    }
 }
 
+// component to render
 ReactDOM.render(<Homepage />, document.querySelector('#app'));
